8b77d62fc7869502a9df4209e7c1afcb1ca45224
husile <husile@gmail.com>
2016-07-29 15:22:24 +0800

    Initial Commit

---
+++ DockExposeHighlight/DockExposeHighlight.m
@@ -0,0 +1,10 @@
+//
+//  DockExposeHighlight.m
+//  DockExposeHighlight
+//
+//  Created by husile on 7/29/16.
+//  Copyright © 2016 husile. All rights reserved.
+//
+
+#import <ParasiteRuntime/ParasiteRuntime.h>
+
